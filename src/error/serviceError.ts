7197fc3ccc10084a79918f21de276a615bbe715e
Inthava XAIYALOUN <coci8750@gmail.com>
2024-04-30 09:07:12 +0700

    Add error helper to reuse in feature handler

---
+++ src/error/serviceError.ts
@@ -0,0 +1,25 @@
+export class ServiceError extends Error {
+  public readonly name: string;
+  constructor(name: string, description: string, isOperational = true) {
+    super(description);
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+    this.name = name;
+    Error.captureStackTrace(this);
+  }
+}
+
+/**
+ * @throws {ServiceError}
+ * */
+export function emptyCollection(name: string, collection: any) {
+  if (collection.length === 0) {
+    throw new ServiceError(name, "Empty collection", false);
+  }
+}
+
+/**
+ * @throws {ServiceError}
+ * */
+export function notFound(name: string, entity: string) {
+  throw new ServiceError(name, `${entity} not found`);
+}
