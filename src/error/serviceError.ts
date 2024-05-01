a63d90fa20ec397e0f3d78cb1a85687ad2b07050
Inthava XAIYALOUN <coci8750@gmail.com>
2024-05-01 12:50:27 +0700

    Update ServiceError Property

--- src/error/serviceError.ts
+++ src/error/serviceError.ts
@@ -1,9 +1,11 @@
 export class ServiceError extends Error {
   public readonly name: string;
+  public readonly isOperational: boolean;
   constructor(name: string, description: string, isOperational = true) {
     super(description);
     Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
     this.name = name;
+    this.isOperational = isOperational;
     Error.captureStackTrace(this);
   }
 }
