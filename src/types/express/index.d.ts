8224ea05e8619a34e87d522f81ad7fd97ecee465
Inthava XAIYALOUN <coci8750@gmail.com>
2024-05-06 15:03:21 +0700

    Update custom Library type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,4 +1,20 @@
+import { response } from "express";
 import session from "express-session";
+interface ValidationErrorBody {
+  type: string;
+  value: any;
+  msg: string;
+  path: string;
+  location: string;
+}
+type BodyValidationError = ValidationErrorBody[] | undefined;
+declare module "express" {
+  interface Response {
+    locals: {
+      validationError?: BodyValidationError;
+    };
+  }
+}
 
 declare module "express-session" {
   interface SessionData {
